cbbdd5728c8b18e7b22437768b24dc193017c2b2
keyorri <162867121+keyorri@users.noreply.github.com>
2024-03-10 10:48:56 +0300

    Add files via upload

---
+++ balls/js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+    
+    $('.load-more').on('click', function(){
+        const btn = $(this);
+        const loader = btn.find('span');
+        
+        $.ajax({
+            url: '../data.html',
+            type:'GET',
+            beforeSend: function(){
+                btn.attr('disabled', true);
+                loader.addClass('d-inline-block')
+            },
+            success: function(responce){
+                btn.attr('disabled', false);
+                loader.removeClass('d-inline-block');
+                console.log(responce);
+            },
+            error: function(){
+                alert('Error');
+                btn.aftr('disabled', false);
+                loader.removeClass('d-inline-block')
+            }
+            
+        });
+        
+    });
+});
+
+
